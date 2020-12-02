a48b5a963f8f20bc772991312ff03816ec233253
sasaotomofumi <tomomasasso@gmail.com>
2020-12-02 17:37:03 +0900

    ルームモデル作成

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :room_users
+  has_many :rooms, through: :room_users
+  has_many :messages
+  has_many :suggestions
+  has_many :share_messages
+
+  validates :name, :position, :occupation, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  get 'messages/index'
+  devise_for :users
    root to: "messages#index"
+   resources :users, only: [:edit, :update, :show] 
+ 
+ resources :suggestions do 
+  resources :share_messages, only: [:index, :create, :destroy, :edit, :update]
+ end
+
+ resources :rooms, only: [:new, :create, :destroy] do
+  resources :messages, only: [:index, :create, :destroy, :edit, :update]
+ end
 end
