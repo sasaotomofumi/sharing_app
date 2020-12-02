bb109bb02ab94e3bcffd41bf37cd6a17aaf36b6c
sasaotomofumi <tomomasasso@gmail.com>
2020-12-02 19:53:37 +0900

    ゲストログイン機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def  show
     @user = User.find(params[:id])
     @messages = Message.includes(:user).order('created_at DESC')
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,11 @@ class User < ApplicationRecord
   has_many :share_messages
 
   validates :name, :position, :occupation, presence: true
+
+  def self.guest 
+    find_or_create_by!(name: 'ゲスト', position: 'ゲスト', occupation: 'ゲスト', email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
 end
