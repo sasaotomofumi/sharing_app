d8edad7a50a4dff00c56ee144199cfb041bd5a53
sasaotomofumi <tomomasasso@gmail.com>
2020-12-09 12:36:46 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'sharing_app'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:sasaotomofumi/sharing-app.git'
+set :repo_url,  'git@github.com:sasaotomofumi/sharing_app.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
